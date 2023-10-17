d184b03f501c805062efb2f0f6ce88eb01c6663e
Vanessa Jesik <vajesik@gmail.com>
2023-10-17 16:47:58 -0600

    Add provider page with add form and delete button

--- client/src/components/ProviderPage.js
+++ client/src/components/ProviderPage.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { Suspense, useState, useEffect } from "react";
+import ProviderCard from "./ProviderCard";
+import ProviderForm from "./ProviderForm";
 
 function ProviderPage() {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const response = await fetch("/providers");
+      const provArr = await response.json();
+      setProviders(provArr);
+    };
+    fetchProviders().catch(console.error);
+  }, []);
+
+  const handleNewProvider = newProvider => {
+    setProviders([...providers, newProvider]);
+  };
+
+  function handleDeleteProvider(id) {
+    fetch(`/providers/${id}`, { method: "DELETE" }).then(r => {
+      if (r.ok) {
+        setProviders(providers =>
+          providers.filter(provider => provider.id !== id)
+        );
+      }
+    });
+  }
+
+  let providerCards = providers.map(provider => (
+    <ProviderCard
+      key={provider.id}
+      provider={provider}
+      onDelete={handleDeleteProvider}
+    />
+  ));
+
   return (
     <div>
       <h1>Provider Portal</h1>
+      <Suspense>
+        <h1>Providers</h1>
+        <div className="providerList">{providerCards}</div>
+      </Suspense>
+      <ProviderForm handleNewProvider={handleNewProvider} />
     </div>
   );
 }
--- server/app.py
+++ server/app.py
@@ -41,9 +41,25 @@ def index():
 
 class Providers(Resource):
     def get(self):
-        return make_response(
-            [provider.to_dict() for provider in Provider.query.all()], 200
-        )
+        try:
+            providers = [provider.to_dict() for provider in Provider.query.all()]
+            return make_response(providers, 200)
+        except Exception as e:
+            return make_response({"error": str(e)}, 500)
+
+    def post(self):
+        provider_json = request.get_json()
+        provider = Provider()
+
+        try:
+            for key in provider_json:
+                if hasattr(provider, key):
+                    setattr(provider, key, provider_json[key])
+            db.session.add(provider)
+            db.session.commit()
+            return make_response(provider.to_dict(), 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
 
 
 class ProviderById(Resource):
@@ -52,6 +68,32 @@ class ProviderById(Resource):
         if provider:
             return make_response(provider.to_dict(), 200)
         return make_response({"error": "Provider not found"}, 404)
+
+    def patch(self, id):
+        provider = db.session.get(Provider, id)
+
+        if provider:
+            provider_json = request.get_json()
+            try:
+                for key in provider_json:
+                    if hasattr(provider, key):
+                        setattr(provider, key, provider_json[key])
+                db.session.commit()
+                return make_response(provider.to_dict(), 202)
+            except ValueError:
+                return make_response({"errors": ["validation errors"]}, 400)
+
+        return make_response({"error": "provider not found"}, 404)
+
+    def delete(self, id):
+        provider = db.session.get(Provider, id)
+
+        if provider:
+            db.session.delete(provider)
+            db.session.commit()
+            return "", 204
+
+        return make_response({"error": "provider not found"}, 404)
 
 
 class Incidents(Resource):
