f2e97735fa3c8965ab7a197c5017a9333d37ccce
Madison <mlinkenmeyer@gmail.com>
2023-10-16 21:17:23 -0700

    Created provider components

---
+++ client/src/components/ProviderPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProviderPage() {
+  return <h1>Provider Portal</h1>;
+}
+
+export default ProviderPage;
---
+++ client/src/components/ProvidersList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProvidersList() {
+  return <h1>Providers</h1>;
+}
+
+export default ProvidersList;
