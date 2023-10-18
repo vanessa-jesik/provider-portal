432dde716007024987bef3500b935d374e205e28
Madison <mlinkenmeyer@gmail.com>
2023-10-18 15:40:36 -0500

    patients displaying on page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar.js";
 import ProviderPage from "./ProviderPage.js";
 import ProviderById from "./ProviderById.js";
+import PatientPage from "./PatientPage.js";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ProviderPage />} />
           <Route path="/providers/:id" element={<ProviderById />} />
+          <Route path="/patients" element={<PatientPage />} />
         </Routes>
       </main>
     </div>
---
+++ client/src/components/PatientPage.js
@@ -0,0 +1,32 @@
+import React, { Suspense, useState, useEffect } from "react";
+import PatientCard from "./PatientCard";
+
+function PatientPage() {
+  const [patients, setPatients] = useState([]);
+
+  useEffect(() => {
+    const fetchPatients = async () => {
+      const response = await fetch("/patients");
+      const patArr = await response.json();
+      setPatients(patArr);
+    };
+    fetchPatients().catch(console.error);
+  }, []);
+
+  let patientCards = patients.map((patient) => (
+    <PatientCard key={patient.id} patient={patient} />
+  ));
+
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-center py-4 bg-blurple-500 text-white">
+        Patients
+      </h1>
+      <Suspense>
+        <h1 className="text-2xl font-semibold mb-4">Patients</h1>
+        <div className="providerList">{patientCards}</div>
+      </Suspense>
+    </div>
+  );
+}
+export default PatientPage;
