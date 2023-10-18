31004d659ec853d884558c27d0c15388fb55b3c6
Madison <mlinkenmeyer@gmail.com>
2023-10-18 11:03:52 -0700

    Color theme to blue-purple, format provider card

--- client/src/components/ProviderCard.js
+++ client/src/components/ProviderCard.js
@@ -6,16 +6,27 @@ function ProviderCard({
   onDelete,
 }) {
   return (
-    <div className="providercard">
-      <h3>{name}</h3>
-      <p>
-        {badge_number} {provider_type}
+    <div className="bg-white rounded-lg p-4 shadow-md">
+      <h3 className="text-xl font-semibold mb-2">{name}</h3>
+      <p className="text-gray-600 mb-2">
+        {provider_type}, #{badge_number}
       </p>
-      <Link to={`/providers/${id}`}>View Provider</Link>
-      <span>
-        {" "}
-        <button>Edit</button>
-        <button onClick={() => onDelete(id)}>Delete</button>
+      <span className="mt-2">
+        <Link
+          to={`/providers/${id}`}
+          className="bg-blurple-500 text-white py-2 px-4 rounded-lg hover:bg-blurple-700"
+        >
+          View Provider
+        </Link>
+        <button className="bg-blurple-600 text-white py-2 px-4 rounded-lg mr-2 hover:bg-blurple-800">
+          Edit
+        </button>
+        <button
+          onClick={() => onDelete(id)}
+          className="bg-blurple-700 text-white py-2 px-4 rounded-lg hover:bg-blurple-900"
+        >
+          Delete
+        </button>
       </span>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blurple: {
+          100: "#E0E7FF",
+          200: "#C7D2FE",
+          300: "#A5B4FC",
+          400: "#818CF8",
+          500: "#6366F1",
+          600: "#4F46E5",
+          700: "#4338CA",
+          800: "#3730A3",
+          900: "#312E81",
+        },
+      },
+    },
   },
   plugins: [],
 };
