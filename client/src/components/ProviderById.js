934d855e3ed477dc7990d4aa8dcd6b0ebe55e633
Vanessa Jesik <vajesik@gmail.com>
2023-10-20 14:24:53 -0600

    render incidents on patientbyid page

--- client/src/components/ProviderById.js
+++ client/src/components/ProviderById.js
@@ -13,18 +13,18 @@ function ProviderById() {
 
   useEffect(() => {
     fetch(`http://localhost:5555/providers/${id}`)
-      .then((r) => r.json())
-      .then((provider) => setProvider(provider))
-      .catch((error) => {
+      .then(r => r.json())
+      .then(provider => setProvider(provider))
+      .catch(error => {
         console.error("Error fetching provider", error);
       });
   }, []);
 
   useEffect(() => {
     fetch("http://localhost:5555/patients")
-      .then((r) => r.json())
-      .then((patients) => setPatients(patients))
-      .catch((error) => {
+      .then(r => r.json())
+      .then(patients => setPatients(patients))
+      .catch(error => {
         console.error("Error fetching patients", error);
       });
   }, []);
@@ -36,7 +36,7 @@ function ProviderById() {
   }
 
   function handleUpdateIncident(updatedIncident) {
-    const updatedIncidents = provider.incidents.map((incident) => {
+    const updatedIncidents = provider.incidents.map(incident => {
       if (incident.id === updatedIncident.id) {
         return updatedIncident;
       }
@@ -49,10 +49,10 @@ function ProviderById() {
   function handleDeleteIncident(incident_id) {
     fetch(`http://localhost:5555/incidents/${incident_id}`, {
       method: "DELETE",
-    }).then((r) => {
+    }).then(r => {
       if (r.ok) {
         const updatedIncidents = provider.incidents.filter(
-          (incident) => incident.id !== incident_id
+          incident => incident.id !== incident_id
         );
         const updatedProvider = { ...provider, incidents: updatedIncidents };
         setProvider(updatedProvider);
@@ -68,7 +68,7 @@ function ProviderById() {
             onClick={() => navigate("/providers")}
             className="bg-barn text-papaya px-4 py-2 mx-4 m-2 rounded-md hover:bg-barn-dark"
           >
-            BACK
+            View All Providers
           </button>
           <div className="bg-papaya-light px-20 py-4 rounded-lg shadow-md">
             <h1 className="text-xl font-semibold text-fire-dark">
@@ -91,7 +91,7 @@ function ProviderById() {
             ) : null
           ) : null}
           <div>
-            {provider.incidents.map((incident) => (
+            {provider.incidents.map(incident => (
               <IncidentCard
                 key={incident.id}
                 incident={incident}
